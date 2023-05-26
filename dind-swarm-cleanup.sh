63f2440349c416ce7988f581198bbc4ef9ffa6df
David Lavieri <daviddlavier@gmail.com>
2023-05-27 00:43:51 +0200

    remove old

--- dind-swarm-cleanup.sh
+++ dind-swarm-cleanup.sh
@@ -2,7 +2,7 @@
 
 # remove containers
 sudo docker container rm -f manager worker-1 worker-2 worker-3
-
+# remove network
 sudo docker network rm swarm
-
+# remove volumes
 sudo docker volume rm some-docker-certs-ca some-docker-certs-client
